c06bc70a1846dd3fe888f2258806eac7d1e99bbb
Lucas Sodre <lucasmeinen@yahoo.com.br>
2024-12-29 16:44:32 -0300

    Criando componente cadastrar-instrutor

---
+++ estudio-fruto/src/app/features/cadastrar-instrutor/cadastrar-instrutor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-instrutor',
+  imports: [],
+  templateUrl: './cadastrar-instrutor.component.html',
+  styleUrl: './cadastrar-instrutor.component.scss'
+})
+export class CadastrarInstrutorComponent {
+
+}
--- estudio-fruto/src/app/features/login/login.component.ts
+++ estudio-fruto/src/app/features/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -14,15 +15,21 @@ export class LoginComponent {
   email: string = '';
   senha: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   fazerLogin(): void {
     // Simulação de validação
-    if (this.email === 'admin@estudio.com' && this.senha === '123456') {
-      alert('Login realizado com sucesso!');
-      this.router.navigate(['/agenda']);
-    } else {
-      alert('E-mail ou senha incorretos.');
-    }
+    const loginData = { email: this.email, senha: this.senha };
+
+    this.http.post('/api/instrutores/login', loginData).subscribe(
+      (response: any) => {
+        alert('Login realizado com sucesso!');
+        localStorage.setItem('instrutor', JSON.stringify(response));
+        this.router.navigate(['/agenda']);
+      },
+      (error) => {
+        alert(error.error || 'Erro ao realizar login. Verifique as credenciais.');
+      }
+    );
   }
 }
