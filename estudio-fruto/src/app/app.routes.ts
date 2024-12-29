3e7bb10b95f43eca0a8a436e6202442dc76cbbed
Lucas Sodre <lucasmeinen@yahoo.com.br>
2024-12-29 17:12:22 -0300

    layout/scss/rotas para cadastrar instrutor

--- estudio-fruto/src/app/app.routes.ts
+++ estudio-fruto/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 
 import { LoginComponent } from './features/login/login.component';
+import { CadastrarInstrutorComponent } from './features/cadastrar-instrutor/cadastrar-instrutor.component'; 
 import { AlunosComponent } from './features/alunos/alunos.component';
 import { AgendaComponent } from './features/agenda/agenda.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
+  {path: 'cadastrar-instrutor', component: CadastrarInstrutorComponent},
   { path: 'agenda', component: AgendaComponent},
   { path: 'alunos', component: AlunosComponent }
 ];
--- estudio-fruto/src/app/features/cadastrar-instrutor/cadastrar-instrutor.component.ts
+++ estudio-fruto/src/app/features/cadastrar-instrutor/cadastrar-instrutor.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-cadastrar-instrutor',
-  imports: [],
+  standalone: true,
   templateUrl: './cadastrar-instrutor.component.html',
-  styleUrl: './cadastrar-instrutor.component.scss'
+  styleUrl: './cadastrar-instrutor.component.scss',
+  imports: [FormsModule, CommonModule, DragDropModule]
 })
 export class CadastrarInstrutorComponent {
+
+  instrutor = {
+    nome: '',
+    email: '',
+    contato: '',
+    senha: ''
+  };
+
+  constructor(private http: HttpClient) {}
+
+  cadastrarInstrutor(): void {
+    this.http.post('/api/instrutores/cadastrar', this.instrutor).subscribe(
+      () => {
+        alert('Instrutor cadastrado com sucesso!');
+        this.instrutor = { nome: '', email: '', contato: '', senha: '' };
+      },
+      (error) => {
+        alert(`Erro ao cadastrar instrutor: ${error.error}`);
+      }
+    );
+  }
 
 }
